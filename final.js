df223e3e0d690d42aa47e2b6ecb627a884f7126e
alin <bonciu_alin@yahoo.com>
2021-05-12 20:27:58 +0300

    Adding big improvements

--- final.js
+++ final.js
@@ -1,52 +1,86 @@
-var myNodelist = document.getElementsByTagName("LI");
+let myNodelist = document.getElementsByTagName("LI");
 
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
+setInterval(function () {
+    let close = document.getElementsByClassName("close");
+    let i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.remove('li')
+        }
+    }
+}, 100);
+
+/* let check = document.getElementsByClassName("check");
+for (i = 0; i < check.length; i++) {
+    check[i].onclick = function () {
+        div.classList.remove("checked");        
+        let div = this.parentElement;
+        let k = document.createElement('img')
+        k.src = "unchecked.png"
+        div.classList.add("uncheck");
+        k.className = 'check'
+        div.appendChild(k);
     }
 }
-
-var check = document.getElementsByClassName("uncheck");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-    }
-}
-
-var input = document.getElementById("myInput");
-document.body.input.onkeyup = function (e) {
-    if (e.key === "Enter") {
+ */
+let input = document.getElementById("myInput");
+input.addEventListener("keyup", function (e) {
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
         newElement()
     }
-}
+});
 
-var unchecked = document.querySelector('uncheck');
+/* let unchecked = document.getElementsByClassName('uncheck');
 unchecked.addEventListener('click', function (ev) {
     if (ev.target.className === 'uncheck') {
-        ev.target.classList.toggle('check');
+        target.classList.remove("uncheck");
+        target.classList.add("checked");
     }
 }, false);
 
-var checked = document.querySelector('check');
+let checked = document.querySelector('check');
 checked.addEventListener('click', function (ev) {
     if (ev.target.className === 'check') {
         ev.target.classList.toggle('uncheck');
     }
-}, false);
+}, false); */
+
+let itemsleft = document.getElementById('number');
 
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    var k = document.querySelector('uncheck')
+    let li = document.createElement("li");
+    li.id = "item"
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    let k = document.createElement('img')
+    let j = document.createElement('img')
+
+    k.src = "unchecked.png"
+    k.className = 'uncheck'
+    let kk = document.createElement('img')
+    kk.src = "checked.png"
+    kk.className = 'check'
+
+    k.addEventListener('click', function () {
+        let div = this.parentElement;
+        div.classList.add("checked");
+        div.appendChild(kk)
+    });
+
+    kk.addEventListener('click', function () {
+        let div = this.parentElement;
+        div.classList.remove("checked");
+        div.removeChild(kk)
+        div.appendChild(k)
+    });
+
+    j.src = "close.png"
+    j.className = 'close'
+
     li.appendChild(k)
     li.appendChild(t);
-    var j = document.querySelector('close')
     li.appendChild(j)
     if (inputValue === '') {
         alert("You must write something!");
@@ -54,12 +88,81 @@ function newElement() {
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
-/*     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
-        }
-    } */
+    itemsleft.textContent = myNodelist.length + " items left"
 }
 
-//    var arrow = document.getElementById("arrow");
+let arrow = document.getElementById("arrow");
+
+arrow.addEventListener('click', function () {
+    let myNodelist = document.getElementsByTagName("LI");
+    let kk = document.createElement('img')
+    kk.src = "checked.png"
+    kk.className = 'check'
+    let k = document.createElement('img')
+    k.src = "unchecked.png"
+    k.className = 'uncheck'
+    let v = 0
+    for (i = 0; i < myNodelist.length; i++)
+        console.log(myNodelist[i])
+        /* if (myNodelist[i].classList.contains("checked")) { v = v + 1 }
+        if (v === i) {
+
+            myNodelist[i].classList.remove("checked");
+            myNodelist[i].removeChild(kk);
+            myNodelist[i].appendChild(k);
+        }
+        else {
+            myNodelist[i].classList.add("checked");
+            myNodelist[i].removeChild(k);
+            myNodelist[i].appendChild(kk);
+        } */
+        if (myNodelist[i].classList.contains("checked"))
+        {}
+        else{
+            myNodelist[i].classList.add("checked");
+            myNodelist[i].removeChild(k);
+            myNodelist[i].appendChild(kk);
+        }
+});
+
+
+let all = document.getElementById('all');
+all.addEventListener('click', function () {
+    for (i = 0; i < myNodelist.length; i++) {
+        myNodelist[i].style.display = "block";
+    }
+    itemsleft.textContent = myNodelist.length + " items left"
+}, false);
+
+let active = document.getElementById('active');
+active.addEventListener('click', function () {
+    let myNodelist = document.getElementsByTagName("li");
+    c = myNodelist.length
+    for (i = 0; i < myNodelist.length; i++) {
+        myNodelist[i].style.display = "block";
+    }
+    for (i = 0; i < myNodelist.length; i++) {
+        if (myNodelist[i].classList.contains("checked")) {
+            myNodelist[i].style.display = "none";
+            c = c-1
+            itemsleft.textContent = c + " items left"
+        }
+    }
+
+});
+
+let completed = document.getElementById('completed');
+completed.addEventListener('click', function () {
+    let myNodelist = document.getElementsByTagName("li");
+    c = myNodelist.length
+    for (i = 0; i < myNodelist.length; i++) {
+        myNodelist[i].style.display = "block";
+    }
+    for (i = 0; i < myNodelist.length; i++) {
+        if (!myNodelist[i].classList.contains("checked")) {
+            myNodelist[i].style.display = "none";
+            c = c-1
+            itemsleft.textContent = c + " items left"
+        }
+    }
+}, false);
